9f4e52757294755d9c471afc7e62c2d924e2841f
Eshwar Ramesh <eshwar@metarain.com>
2014-06-16 11:20:46 +0530

    trying to synchronize project remote master with local

--- Quote/TodayViewController.swift
+++ Quote/TodayViewController.swift
@@ -28,11 +28,28 @@ class TodayViewController: UIViewController {
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
         // Perform any setup necessary in order to update the view.
-
+        
         // If an error is encoutered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
-
+        /*var url = NSURL(string:QUERY_URL)
+        var urlRequest = NSURLRequest(URL:url)
+        var vcInstance:ViewController = ViewController.shared() as ViewController
+        vcInstance.getData(urlRequest,  {(data:NSData?, response:NSURLResponse?, error:NSError?) in
+        dispatch_async(dispatch_get_main_queue(), {
+        if error {
+        println("Error retrieving data for extension")
+        self.result = NCUpdateResult.Failed
+        }
+        else {
+        var dict:NSDictionary? = vcInstance.parseJSON(data!)
+        self.strQuote = dict!["quote"] as? NSString
+        self.displayLabel.text = self.strQuote
+        println("display label text : \(self.displayLabel.text)")
+        self.result = NCUpdateResult.NewData
+        }
+        })
+        })*/
         completionHandler(NCUpdateResult.NewData)
     }
     
